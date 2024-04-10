fe8075ce7ffce586ab7553574e98b1537da1efec
lai2-24 <lai2-24@itismeucci.com>
2024-04-10 13:27:12 +0200

    funge

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import Ciccio from './Ciccio.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+    <Ciccio nome="Giovanni" />
+    <Ciccio eta="19" />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ app/src/Ciccio.js
@@ -0,0 +1,19 @@
+import './Ciccio.css';
+import {useState} from 'react';
+
+function Ciccio({nome, eta}) {
+
+
+    const [contatore, setContatore] = useState(0);
+    function gestisciClick(){
+        setContatore(contatore +1);
+    }
+  return (
+    <div className="rosso">    
+        Ciao a tutti sono {nome} {eta}
+        <button onClick={gestisciClick}>{contatore}</button>
+    </div>
+  );
+}
+
+export default Ciccio;
